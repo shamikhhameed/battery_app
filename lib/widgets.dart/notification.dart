91f1213cc076c50ead39320c8c7f33e96534888c
ShehaniFernando <it19011912@my.sliit.lk>
2022-04-03 14:08:45 +0530

    Added Comments for the Notification Function

--- lib/widgets.dart/notification.dart
+++ lib/widgets.dart/notification.dart
@@ -1,40 +1,47 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+//NOTIFICATION PLUGIN
 
+//Create an instance of Local notifications
 void sendNotification({String? title, String? body}) async {
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-  FlutterLocalNotificationsPlugin();
+      FlutterLocalNotificationsPlugin();
 
-  //Set the settings for various platforms
-
-// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+//Set the settings for various platforms
+  // Initialise the plugin
+  //For the Android Initialization
   const AndroidInitializationSettings initializationSettingsAndroid =
-  AndroidInitializationSettings('@mipmap/ic_launcher');
+      AndroidInitializationSettings('@mipmap/ic_launcher');
 
+  //For the IOS Initialization
   const IOSInitializationSettings initializationSettingsIOS =
-  IOSInitializationSettings(
-      requestAlertPermission: true,
-      requestSoundPermission: true,
-      requestBadgePermission: true);
+      IOSInitializationSettings(
+          requestAlertPermission: true,
+          requestSoundPermission: true,
+          requestBadgePermission: true);
+
+  //For the Linux Initialization
   const LinuxInitializationSettings initializationSettingsLinux =
-  LinuxInitializationSettings(defaultActionName: 'hello');
-  const InitializationSettings initializationSettings =
-  InitializationSettings(
+      LinuxInitializationSettings(defaultActionName: 'hello');
+
+  //Initialize and register the settings
+  const InitializationSettings initializationSettings = InitializationSettings(
       android: initializationSettingsAndroid,
       iOS: initializationSettingsIOS,
       linux: initializationSettingsLinux);
+
+  //Initialize the flutter notifications (package)
   await flutterLocalNotificationsPlugin.initialize(
     initializationSettings,
   );
 
-  //
+  //Register the notification channel to the Android device
   const AndroidNotificationChannel channel = AndroidNotificationChannel(
       'high channel', 'High Importance notification',
-      description: "This channel is use for important notification",
+      description: "This channel is used for important notifications",
       importance: Importance.max);
 
-  //
-
+  //Sends the notification
   flutterLocalNotificationsPlugin.show(
     0,
     title,
