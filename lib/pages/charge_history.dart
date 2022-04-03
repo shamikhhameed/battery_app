d93bb4da0260419e3c28904e99fc99baa81547ed
ShamikhHameed <63804919+ShamikhHameed@users.noreply.github.com>
2022-04-03 21:53:11 +0530

    updated code

--- lib/pages/charge_history.dart
+++ lib/pages/charge_history.dart
@@ -12,7 +12,6 @@ class ChargeHistory extends StatefulWidget {
 }
 
 class _ChargeHistoryState extends State<ChargeHistory> {
-  //all charge history details
   List<Map<String, dynamic>> _chargeHistory = [];
   Battery battery = Battery();
   int showBatteryLevels = 0;
@@ -48,14 +47,14 @@ class _ChargeHistoryState extends State<ChargeHistory> {
           batteryState = event;
       });
     }
-      if (batteryState == BatteryState.charging) {
-        // _addItem();
-        _addOrUpdateItem();
-        _deleteItem();
-      }
+    if (batteryState == BatteryState.charging) {
+      _addOrUpdateItem();
+      _deleteItem();
+    }
     });
   }
 
+  // Checks for the battery percentage every 5 seconds and sets the state accordingly
   _broadcastBatteryLevels() async {
     broadcastBattery = true;
     while (broadcastBattery!) {
@@ -69,41 +68,33 @@ class _ChargeHistoryState extends State<ChargeHistory> {
     }
   }
 
-  // Insert a new charge data to the database
+  // Insert a new row to the database when the charger is connected
   Future<void> _addItem() async {
     var batteryLevel = await battery.batteryLevel;
     await SQLHelper.createItem(batteryLevel.toString());
     _refreshChargeHistory();
   }
 
-  //Update an existing journal
+  // Updates an existing record in the database
   Future<void> _updateItem() async {
     var batteryLevel = await battery.batteryLevel;
     await SQLHelper.updateItem(batteryLevel.toString());
     _refreshChargeHistory();
   }
 
-  // Delete an item
+  // Delete an existing record from the database
   void _deleteItem() async {
     await SQLHelper.deleteItem();
     _refreshChargeHistory();
   }
 
+  // Adds a new record if a record does not exist for the particular date or else updates the record
   Future<void> _addOrUpdateItem() async {
-    var data;
-    print(data == null
-        ? 'data before getItem = NULL'
-        : 'data before getItem = ' + data.toString());
+    List<Map<String, dynamic>> data;
     data = await SQLHelper.getItem();
-    print(data == null
-        ? 'data after getItem = NULL'
-        : 'data after getItem = ' + data.toString());
-    // print('data after getItem ' + data);
     if (data.isEmpty) {
-      print('saying data NULL');
       await _addItem();
     } else {
-      print('saying data NOT NULL');
       await _updateItem();
     }
   }
@@ -116,117 +107,78 @@ class _ChargeHistoryState extends State<ChargeHistory> {
         centerTitle: true,
       ),
       body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: _chargeHistory.length,
-              itemBuilder: (context, index) => Card(
-                    margin: const EdgeInsets.all(7),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: ListTile(
-                      title: Text('Charger was connecetd at ' +
-                          _chargeHistory[index]['percentage'] +
-                          '%'),
-                      // title: Transform.translate(
-                      //   offset: const Offset(0, 0),
-                      //   child: Text('Charger was connecetd at ' + _chargeHistory[index]['percentage'] + '%'),
-                      // ),
-                      // subtitle: Text('On ' + _chargeHistory[index]['createdAt'].split(' ')[0] + '\nAt ' + _chargeHistory[index]['createdAt'].split(' ')[1]),
-                      subtitle: Transform.translate(
-                        offset: const Offset(0, 2),
-                        child: Text('On ' +
-                            _chargeHistory[index]['createdAt'].split(' ')[0] +
-                            '\nAt ' +
-                            _chargeHistory[index]['createdAt'].split(' ')[1]),
-                      ),
-                      isThreeLine: true,
-                      trailing: Container(
-                        // margin: const EdgeInsets.only(top: 50),
-                        // width: MediaQuery.of(context).size.width * 0.5,
-                        // height: MediaQuery.of(context).size.width * 0.3,
-                        width: 60,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(200),
-                          // boxShadow: [
-                          //   BoxShadow(
-                          //       blurRadius: 5,
-                          //       spreadRadius: 2,
-                          //       offset: const Offset(0, 0),
-                          //       // blurRadius: 7,
-                          //       // spreadRadius: -5,
-                          //       // offset: const Offset(4, 4),
-                          //       color: COLOR_GREY
-                          //       ),
-                          // ]
-                        ),
-                        child: SfRadialGauge(
-                          axes: [
-                            RadialAxis(
-                              minimum: 0,
-                              maximum: 100,
-                              startAngle: 270,
-                              endAngle: 270,
-                              showLabels: false,
-                              showTicks: false,
-                              axisLineStyle: AxisLineStyle(
-                                  thickness: 1,
-                                  color: double.parse(_chargeHistory[index]
-                                              ['percentage']) <=
-                                          20
-                                      ? COLOR_RED
-                                      : COLOR_GREEN,
-                                  thicknessUnit: GaugeSizeUnit.factor),
-                              pointers: <GaugePointer>[
-                                RangePointer(
-                                  enableAnimation: true,
-                                  animationDuration: 1800,
-                                  value: double.parse(_chargeHistory[index]
-                                          ['percentage']
-                                      .toString()),
-                                  width: 0.3,
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : ListView.builder(
+            itemCount: _chargeHistory.length,
+            itemBuilder: (context, index) => Card(
+              margin: const EdgeInsets.all(7),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: ListTile(
+                title: Text('Charger was connecetd at ' + _chargeHistory[index]['percentage'] + '%'),
+                subtitle: Transform.translate(
+                  offset: const Offset(0, 2),
+                  child: Text('On ' + _chargeHistory[index]['createdAt'].split(' ')[0] + '\nAt ' + _chargeHistory[index]['createdAt'].split(' ')[1]),
+                ),
+                isThreeLine: true,
+                trailing: Container(
+                  width: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(200),
+                  ),
+                  child: SfRadialGauge(
+                    axes: [
+                      RadialAxis(
+                        minimum: 0,
+                        maximum: 100,
+                        startAngle: 270,
+                        endAngle: 270,
+                        showLabels: false,
+                        showTicks: false,
+                        axisLineStyle: AxisLineStyle(
+                            thickness: 1,
+                            color:
+                                double.parse(_chargeHistory[index]['percentage']) <= 20 ? COLOR_RED : COLOR_GREEN,
+                            thicknessUnit: GaugeSizeUnit.factor),
+                        pointers: <GaugePointer>[
+                          RangePointer(
+                            enableAnimation: true,
+                            animationDuration: 1800,
+                            value: double.parse(_chargeHistory[index]['percentage'].toString()),
+                            width: 0.3,
+                            color: Colors.white,
+                            pointerOffset: 0.1,
+                            cornerStyle: showBatteryLevels == 100
+                                ? CornerStyle.bothFlat
+                                : CornerStyle.endCurve,
+                            sizeUnit: GaugeSizeUnit.factor,
+                          )
+                        ],
+                        annotations: <GaugeAnnotation>[
+                          GaugeAnnotation(
+                              positionFactor: 0.5,
+                              angle: 90,
+                              widget: Text(
+                                showBatteryLevels == null
+                                    ? "0"
+                                    : _chargeHistory[index]['percentage'].toString(),
+                                style: const TextStyle(
+                                  fontSize: 15,
                                   color: Colors.white,
-                                  pointerOffset: 0.1,
-                                  cornerStyle: showBatteryLevels == 100
-                                      ? CornerStyle.bothFlat
-                                      : CornerStyle.endCurve,
-                                  sizeUnit: GaugeSizeUnit.factor,
-                                )
-                              ],
-                              annotations: <GaugeAnnotation>[
-                                GaugeAnnotation(
-                                    positionFactor: 0.5,
-                                    angle: 90,
-                                    widget: Text(
-                                      showBatteryLevels == null
-                                          ? "0"
-                                          : _chargeHistory[index]['percentage']
-                                              .toString(),
-                                      style: const TextStyle(
-                                        fontSize: 15,
-                                        color: Colors.white,
-                                      ),
-                                    ))
-                              ],
+                                ),
+                              )
                             )
-                          ],
-                        ),
-                      ),
-                    ),
-                  )),
-      // Column(
-      //   mainAxisAlignment: MainAxisAlignment.center,
-      //   crossAxisAlignment: CrossAxisAlignment.center,
-      //   children: const [
-      //     Text(
-      //       "Charge History including charging details of past 10 days - Shamikh",
-      //       textAlign: TextAlign.center,
-      //     )
-      //   ],
-      // ),
-      //test
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ),
     );
   }
 }
